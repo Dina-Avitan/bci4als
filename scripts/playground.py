b21e79219a529ce13c199fce85fb7674eb1c6d78
seroy123 <80485523+seroy123@users.noreply.github.com>
2022-01-09 16:17:45 +0200

    played with parameters
    got 89%. seems that the csp features arent doing any good

--- scripts/playground.py
+++ scripts/playground.py
@@ -6,12 +6,15 @@ from tkinter import filedialog, Tk
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from mne.decoding import CSP
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.feature_selection import SelectKBest, chi2, mutual_info_classif
 import scipy
 import scipy.io
 from bci4als import ml_model
 from sklearn import svm
 from sklearn.model_selection import cross_val_score
+from sklearn.pipeline import Pipeline
 
 
 
@@ -27,16 +30,24 @@ def playground():
 
 
 def load_eeg():
+    fs = 500
+    bands = np.matrix('7 12; 12 15; 17 22; 25 30; 7 35; 30 35')
+    max_score = 1
+    clf = svm.SVC(decision_function_shape='ovo', kernel='linear')
+
+    # Ofir's data
     EEG = scipy.io.loadmat(r'C:\Users\User\Desktop\ALS_BCI\team13\bci4als-master\bci4als\scripts\EEG.mat')
     trainingVec = scipy.io.loadmat(r'C:\Users\User\Desktop\ALS_BCI\team13\bci4als-master\bci4als\scripts\trainingVec.mat')
     data = EEG['EEG']
     labels = np.ravel(trainingVec['trainingVec'].T)
-    bands = np.matrix('8 12; 16 22; 30 35')
-    fs = 500
-    #  data should be trails X electrodes X samples.
+     # data should be trails X electrodes X samples.
     data = np.transpose(data, (2, 0, 1))
-    clf = svm.SVC(decision_function_shape='ovo', kernel='linear')
-    max_score = 1
+
+    # Assemble a classifier
+    lda = LinearDiscriminantAnalysis()
+    csp = CSP(n_components=4, reg=None, log=False, norm_trace=False, transform_into='average_power', cov_est='epoch')
+    csp_features = Pipeline([('CSP', csp), ('LDA', lda)]).fit_transform(data, labels)
+
     final_data = []
     for trial in range(data.shape[0]):
         # C4
@@ -52,44 +63,28 @@ def load_eeg():
         else:
             final_data = np.vstack((final_data, new_data[np.newaxis]))
     data = final_data
-    for feat_num in range(1, 10):
-        bandpower_features_new = ml_model.MLModel.bandpower(data, bands, fs, window_sec=1, relative=False)
-        bandpower_features_rel = ml_model.MLModel.bandpower(data, bands, fs, window_sec=0.1, relative=True)
+
+    # Our data
+    # data1 = pd.read_pickle(r'C:\Users\User\Desktop\ALS_BCI\team13\bci4als-master\bci4als\recordings\noam\2\raw_model.pickle')
+    # data2 = pd.read_pickle(r'C:\Users\User\Desktop\ALS_BCI\team13\bci4als-master\bci4als\recordings\noam\3\raw_model.pickle')
+    # data = np.concatenate((data1.epochs.get_data()[:, :, :550], data2.epochs.get_data()[:, :, :550]), axis=0)
+    # labels = np.concatenate((data1.labels,data2.labels), axis=0)
+
+    for feat_num in range(1, int(math.sqrt(data.shape[0]))):
+        bandpower_features_new = ml_model.MLModel.bandpower(data, bands, fs, window_sec=0.5, relative=False)
+        # bandpower_features_rel = ml_model.MLModel.bandpower(data, bands, fs, window_sec=0.9, relative=True)
         bandpower_features_old = ml_model.MLModel.hjorthMobility(data)
 
-        bandpower_features_wtf = np.concatenate((bandpower_features_new, bandpower_features_old), axis=1)
+        bandpower_features_wtf = np.concatenate((bandpower_features_new, bandpower_features_old, csp_features), axis=1)
         bandpower_features_wtf = scipy.stats.zscore(bandpower_features_wtf)
         bandpower_features_wtf = SelectKBest(mutual_info_classif, k=feat_num).fit_transform(bandpower_features_wtf, labels)
         scores_mix = cross_val_score(clf, bandpower_features_wtf, labels, cv=8)
         (print(f"Prediction rate is: {np.mean(scores_mix)*100}%"))
 
-        # bandpower_features_new = SelectKBest(chi2, k=int(math.sqrt(bandpower_features_new.shape[0]))).fit_transform(bandpower_features_new, labels)
-        # scores = cross_val_score(clf, bandpower_features_new, labels, cv=8)
-        # # (print(f"Prediction rate is: {np.mean(scores)*100}%"))
-        #
-        # bandpower_features_old = SelectKBest(chi2, k=int(math.sqrt(bandpower_features_old.shape[0]))).fit_transform(bandpower_features_old, labels)
-        # scores = cross_val_score(clf, bandpower_features_old, labels, cv=8)
-        # # (print(f"Prediction rate is: {np.mean(scores)*100}%"))
-        #
-        # bandpower_features_rel = SelectKBest(chi2, k=int(math.sqrt(bandpower_features_new.shape[0]))).fit_transform(bandpower_features_rel, labels)
-        # scores = cross_val_score(clf, bandpower_features_rel, labels, cv=8)
-        # # (print(f"Prediction rate is: {np.mean(scores)*100}%"))
-
         if np.mean(scores_mix)*100 > max_score:
             max_score = np.mean(scores_mix)*100
             feat_num_max = feat_num
     print(max_score, feat_num_max)
-
-    # clf = ExtraTreesClassifier(n_estimators=50)
-    # clf = clf.fit(bandpower_features, labels)
-    # # clf.feature_importances_
-    # lsvc = LinearSVC(C=0.01, penalty="l1", dual=False).fit(bandpower_features, labels)
-    # # model = SelectFromModel(lsvc, prefit=True)
-    # model = SelectFromModel(clf, prefit=True)
-    # X_new = model.transform(bandpower_features)
-    # clf = svm.SVC(decision_function_shape='ovo', kernel='linear')
-    # scores = cross_val_score(clf, X_new, labels, cv=5)
-    # (print(f"Prediction rate is: {np.mean(scores)*100}%"))
 
 
 def visualize_svm(X, y):
@@ -138,5 +133,5 @@ def visualize_svm(X, y):
     plt.show()
 
 if __name__ == '__main__':
-    playground()
-    # load_eeg()
+    # playground()
+    load_eeg()
