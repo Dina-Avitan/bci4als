dd4aaba6a04590f9d3a453d354063b7a1bb3a632
seroy123 <80485523+seroy123@users.noreply.github.com>
2022-01-20 18:31:29 +0200

    playing with parameters

--- scripts/playground.py
+++ scripts/playground.py
@@ -47,8 +47,13 @@ def load_eeg():
     # perm_ofir
     # perm_c3 = (8, 2, 14, 7, 9, 4, 5, 3, 0, 10)
     # perm our weird combo
-    perm_c3 = (0, 1, 2, 3, 7, 9, 4, 5, 6, 8, 10)
-    # perm_c3 = [8, 9, 6, 5, 3, 10, 2, 7, 4, 1, 0]
+    # perm_c3 = (0, 1, 2, 3, 7, 9, 4, 5, 6, 8, 10) # 63%
+    #more
+    # perm_c3 = (0, 1, 2, 3, 5, 6, 4, 7, 8, 9, 10) # 61
+    # perm_c3 = (0, 1, 2, 3, 5, 6, 4, 7, 9, 8, 10) # 61
+    # perm_c3 = (0, 1, 2, 3, 4, 6, 5, 9, 7, 8, 10)
+    # no bp
+    perm_c3 = (9, 10, 3, 4, 1, 2, 0, 6, 7, 8, 5)
 
     # # Ofir's data
     # EEG = scipy.io.loadmat(r'C:\Users\User\Desktop\ALS_BCI\team13\bci4als-master\bci4als\scripts\EEG.mat')
@@ -110,13 +115,16 @@ def permutation_func():
     fs = 125
     bands = np.matrix('7 12; 12 15; 17 22; 25 30; 7 35; 30 35')
     max_score = 1
-    clf = MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=200, random_state=0, max_iter=800)
-    # clf = svm.SVC(decision_function_shape='ovo', kernel='linear')
-    data2 = pd.read_pickle(r'C:\Users\User\Desktop\ALS_BCI\team13\bci4als-master\bci4als\recordings\noam\8\unfiltered_model.pickle')
+    # clf = MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=200, random_state=0, max_iter=800)
+    clf = svm.SVC(decision_function_shape='ovo', kernel='linear')
+    data2 = pd.read_pickle(r'C:\Users\User\Desktop\ALS_BCI\team13\bci4als-master\bci4als\recordings\roy\2\unfiltered_model.pickle')
     labels = data2.labels
     combinations = list(permutations(range(11)))
-    counter = 1
-    for perm_c3 in combinations[500000:]:
+    counter = 0
+    add = 2
+    unique_check = 0
+    while counter < len(combinations):
+        perm_c3 = combinations[counter]
         data = copy.deepcopy(data2.epochs.get_data())
         final_data = []
         # perm_c3 = (0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 7)
@@ -140,68 +148,77 @@ def permutation_func():
                 final_data = np.vstack((final_data, new_data[np.newaxis]))
         data = final_data
         bandpower_features_new = ml_model.MLModel.bandpower(data, bands, fs, window_sec=0.5, relative=False)
+        bandpower_features_rel = ml_model.MLModel.bandpower(data, bands, fs, window_sec=0.5, relative=True)
         bandpower_features_old = ml_model.MLModel.hjorthMobility(data)
-        bandpower_features_wtf = np.concatenate((bandpower_features_new, bandpower_features_old), axis=1)
+        bandpower_features_wtf = np.concatenate((bandpower_features_new, bandpower_features_old, bandpower_features_rel
+                                                 ), axis=1)
         scaler = StandardScaler()
         scaler.fit(bandpower_features_wtf)
         scaler.transform(bandpower_features_wtf)
-        bandpower_features_wtf = SelectKBest(mutual_info_classif, k=15).fit_transform(bandpower_features_wtf, labels)
+        bandpower_features_wtf = SelectKBest(mutual_info_classif, k=9).fit_transform(bandpower_features_wtf, labels)
         scores_mix = cross_val_score(clf, bandpower_features_wtf, labels, cv=8)
-        print(np.mean(scores_mix)*100)
-        if counter % 100000 == 0:
+        if counter // 10000 >= 1 and counter // 10000 != unique_check:
             print(counter)
+            unique_check = counter // 10000
         if np.mean(scores_mix)*100 > max_score:
             max_score = np.mean(scores_mix)*100
             feat_num_max = 9
             print(f"Prediction rate is: {np.mean(scores_mix) * 100}%")
             print(max_score, feat_num_max, perm_c3)
+        elif (max_score - np.mean(scores_mix)*100) > 3:
+            if add < 200:
+                add = add**2
+            else:
+                add *= 2
+            counter += add
+        elif (max_score - np.mean(scores_mix)*100) <= 3:
+            add = 2
+            counter += add
+        counter += 1
 
-def visualize_svm(X, y):
-    X = SelectKBest(chi2, k=2).fit_transform(X, y)
-    h = .02  # step size in the mesh
-    C = 1.0  # SVM regularization parameter
-    svc = svm.SVC(kernel='linear', C=C).fit(X, y)
-    rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(X, y)
-    poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(X, y)
-    lin_svc = svm.LinearSVC(C=C).fit(X, y)
+def sort_perm():
+    combinations = list(permutations(range(11)))[0:1000]
+    ans = sorted(combinations, key=dunno, reverse=True)
+    print(ans[0:10])
 
-    # create a mesh to plot in
-    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                         np.arange(y_min, y_max, h))
+def dunno(perm_c3):
+    fs = 125
+    bands = np.matrix('7 12; 12 15; 17 22; 25 30; 7 35; 30 35')
+    clf = svm.SVC(decision_function_shape='ovo', kernel='linear')
+    data2 = pd.read_pickle(r'C:\Users\User\Desktop\ALS_BCI\team13\bci4als-master\bci4als\recordings\roy\2\unfiltered_model.pickle')
+    labels = data2.labels
+    data = copy.deepcopy(data2.epochs.get_data())
+    final_data = []
+    for trial in range(data.shape[0]):
+        # C3
+        data[trial][perm_c3[0]] -= (data[trial][perm_c3[1]] + data[trial][perm_c3[2]] + data[trial][perm_c3[3]] +
+                                    data[trial][perm_c3[4]]) / 4
 
-    # title for the plots
-    titles = ['SVC with linear kernel',
-              'LinearSVC (linear kernel)',
-              'SVC with RBF kernel',
-              'SVC with polynomial (degree 3) kernel']
+        # C4
+        data[trial][perm_c3[5]] -= (data[trial][perm_c3[6]] + data[trial][perm_c3[7]] + data[trial][perm_c3[8]] +
+                                    data[trial][perm_c3[9]]) / 4
+        new_data = np.delete(data[trial], [perm_c3[point] for point in [1, 2, 3, 4, 6, 7, 8, 9]], axis=0)
+        if trial == 0:
+            final_data = new_data[np.newaxis]
+        else:
+            final_data = np.vstack((final_data, new_data[np.newaxis]))
+    data = final_data
+    # bandpower_features_new = ml_model.MLModel.bandpower(data, bands, fs, window_sec=0.5, relative=False)
+    # bandpower_features_rel = ml_model.MLModel.bandpower(data, bands, fs, window_sec=0.5, relative=True)
+    bandpower_features_old = ml_model.MLModel.hjorthMobility(data)
+    lzc_features = ml_model.MLModel.LZC(data)
+    dfa_features = ml_model.MLModel.DFA(data)
 
-    for i, clf in enumerate((svc, lin_svc, rbf_svc, poly_svc)):
-        # Plot the decision boundary. For that, we will assign a color to each
-        # point in the mesh [x_min, x_max]x[y_min, y_max].
-        plt.subplot(2, 2, i + 1)
-        plt.subplots_adjust(wspace=0.4, hspace=0.4)
-
-        Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-
-        # Put the result into a color plot
-        Z = Z.reshape(xx.shape)
-        plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
-
-        # Plot also the training points
-        plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm)
-        plt.xlabel('Sepal length')
-        plt.ylabel('Sepal width')
-        plt.xlim(xx.min(), xx.max())
-        plt.ylim(yy.min(), yy.max())
-        plt.xticks(())
-        plt.yticks(())
-        plt.title(titles[i])
-
-    plt.show()
+    bandpower_features_wtf = np.concatenate((lzc_features, dfa_features, bandpower_features_old), axis=1)
+    scaler = StandardScaler()
+    scaler.fit(bandpower_features_wtf)
+    scaler.transform(bandpower_features_wtf)
+    bandpower_features_wtf = SelectKBest(mutual_info_classif, k=8).fit_transform(bandpower_features_wtf, labels)
+    scores_mix = cross_val_score(clf, bandpower_features_wtf, labels, cv=8)
+    return np.mean(scores_mix) * 100
 
 if __name__ == '__main__':
     # playground()
-    load_eeg()
-    # permutation_func()
+    # load_eeg()
+    permutation_func()
+    # sort_perm()
