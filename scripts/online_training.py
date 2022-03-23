69c9431133f51dc85416ffc83097cf4dbc6aca57
noamashk <noamashk@post.bgu.ac.il>
2022-03-23 17:33:41 +0200

    avi

--- scripts/online_training.py
+++ scripts/online_training.py
@@ -30,8 +30,8 @@ def run_experiment(model_path: str):
     eeg = EEG(board_id=CYTON_DAISY, config_json_converted=configurations)
     # If mode= 'practice': It will skip after skip_after errors. it will skip after threshold successes
     # If mode= 'test': It will not skip. It will run skip_after times whether you succeed or fail the trial
-    exp = OnlineExperiment(eeg=eeg, model=model, num_trials=9, buffer_time=buffer_time, threshold=3, skip_after=5,
-                           co_learning=True, debug=False, mode='test',stim_sound=True)
+    exp = OnlineExperiment(eeg=eeg, model=model, num_trials=9, buffer_time=buffer_time, threshold=3, skip_after=4,
+                           co_learning=True, debug=False, mode='test',stim_sound=False)
 
     exp.run(use_eeg=True, full_screen=True)
 
